5b6a88c1a7f034e469ecb84aa8c3693e60bd467b
RP <raviponda@gmail.com>
2020-10-12 16:04:04 -0400

    Set client APIs and Commit Endpoint

--- app/src/main/java/clienApi/ClientApi.java
+++ app/src/main/java/clienApi/ClientApi.java
@@ -1,4 +1,21 @@
 package clienApi;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ClientApi {
+
+    public static final String BASE_URL = "https://api.github.com/";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
--- app/src/main/java/clienApi/GitHubCommitEndPoint.java
+++ app/src/main/java/clienApi/GitHubCommitEndPoint.java
@@ -1,4 +1,17 @@
 package clienApi;
 
+import java.util.List;
+
+import model.GitHubCommit;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public class GitHubCommitEndPoint {
+
+    public interface GitCommitEndPoint {
+
+        @GET("/repos/{user}/{repo}/commits")
+        Call<List<GitHubCommit>> getCommits(@Path("user") String name, @Path("repo") String repos);
+    }
 }
