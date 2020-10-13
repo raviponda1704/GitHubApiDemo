be2d859ffc92da924c65a4e6bedfd25853e60cbe
RP <raviponda@gmail.com>
2020-10-13 19:24:36 -0400

    Set Commit APi

--- app/src/main/java/clienApi/GitHubCommitEndPoint.java
+++ app/src/main/java/clienApi/GitHubCommitEndPoint.java
@@ -7,11 +7,10 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-public class GitHubCommitEndPoint {
 
-    public interface GitCommitEndPoint {
+public interface GitHubCommitEndPoint {
 
         @GET("/repos/{user}/{repo}/commits")
         Call<List<GitHubCommit>> getCommits(@Path("user") String name, @Path("repo") String repos);
-    }
+
 }
